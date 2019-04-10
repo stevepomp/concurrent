d4f4c87f230409ec9ca20a42158749f9b7929ac0
raoyunxiao <raoyunxiao@gmail.com>
2019-04-10 21:08:14 +0800

    chapter03

--- src/main/java/chapter03/ThreadInterrupted.java
+++ src/main/java/chapter03/ThreadInterrupted.java
@@ -1,4 +1,23 @@
 package chapter03;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadInterrupted {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (i <= 1_000) {
+                    i++;
+                    System.out.println(Thread.interrupted());
+                }
+            }
+        };
+//        thread.setDaemon(true);
+        thread.start();
+
+        TimeUnit.MILLISECONDS.sleep(2);
+        thread.interrupt();
+    }
 }
